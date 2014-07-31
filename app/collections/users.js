5e4938ea9c409838553c19c9682fe921f8ae8911
Ivan Nosov <i.k.nosov@gmail.com>
2014-07-31 23:56:22 +0300

    user collection/model

--- app/collections/users.js
+++ app/collections/users.js
@@ -3,6 +3,12 @@ var User = require('../models/user')
 
 module.exports = Base.extend({
   model: User,
-  url: '/users'
+  url: '/users',
+  parse: function(res) {
+    this.itemsPerPage = res.itemsPerPage;
+    this.currentPage = res.currentPage;
+    this.totalItems = res.totalItems;
+    return res.data;
+  }
 });
 module.exports.id = 'Users';
--- app/models/user.js
+++ app/models/user.js
@@ -1,7 +1,7 @@
 var Base = require('./base');
 
 module.exports = Base.extend({
-  url: '/users/:login',
-  idAttribute: 'login'
+  url: '/users/:id',
+  idAttribute: '_id'
 });
 module.exports.id = 'User';
