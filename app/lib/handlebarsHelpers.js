324788388f8d28be72cd7c813ad8547cc26f80f3
Ivan Nosov <i.k.nosov@gmail.com>
2014-07-31 23:53:15 +0300

    add date format helper

--- app/lib/handlebarsHelpers.js
+++ app/lib/handlebarsHelpers.js
@@ -6,6 +6,19 @@ module.exports = function(Handlebars) {
   return {
     copyright: function(year) {
       return new Handlebars.SafeString("&copy;" + year);
+    },
+    dateFormat: function(date) {
+        function leadingZero(number) {
+            return ('0' + number).slice(-2);
+        }
+
+        date = new Date(date || Date.now());
+        return date.getFullYear() + '-'
+            + leadingZero(date.getMonth() + 1) + '-'
+            + leadingZero(date.getDate()) + ' '
+            + leadingZero(date.getHours()) + ':'
+            + leadingZero(date.getMinutes()) + ':'
+            + leadingZero(date.getSeconds());
     }
   };
 };
