c4d8f9e20eea931e0c8fcf4a33a70a7f0f561e89
Ivan Nosov <i.k.nosov@gmail.com>
2014-08-02 00:29:53 +0300

    cleanup

--- app/collections/users.js
+++ app/collections/users.js
@@ -1,14 +1,14 @@
 var User = require('../models/user')
-  , Base = require('./base');
+  , Base = require('./base')
+  , Url = require('url');
 
 module.exports = Base.extend({
   model: User,
-  url: '/users',
-  parse: function(res) {
-    this.itemsPerPage = res.itemsPerPage;
-    this.currentPage = res.currentPage;
-    this.totalItems = res.totalItems;
-    return res.data;
-  }
+    url: function() {
+        // @todo: do it in better way
+        // server fetcher does not apply params
+        return Url.format({ pathname: '/users', query: this.params });
+    },
+  jsonKey: 'data'
 });
 module.exports.id = 'Users';
