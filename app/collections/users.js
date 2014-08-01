c9d546d478a1bc450272d47680be1dae950fab26
Ivan Nosov <i.k.nosov@gmail.com>
2014-08-02 00:38:17 +0300

    apply params only for server

--- app/collections/users.js
+++ app/collections/users.js
@@ -1,13 +1,17 @@
 var User = require('../models/user')
   , Base = require('./base')
-  , Url = require('url');
+  , Url = require('url')
+  , isServer = (typeof window === 'undefined');
 
 module.exports = Base.extend({
   model: User,
   url: function() {
     // @todo: do it in better way
     // server fetcher does not apply params
-    return Url.format({ pathname: '/users', query: this.params });
+    if (isServer) {
+      return Url.format({ pathname: '/users', query: this.params });
+    }
+    return '/users';
   },
   jsonKey: 'data'
 });
