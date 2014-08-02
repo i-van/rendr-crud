954a6516dcf98fded8268d95529e522a0d1151fa
Ivan Nosov <i.k.nosov@gmail.com>
2014-08-02 17:56:15 +0300

    move logic to create view

--- app/views/users/create.js
+++ app/views/users/create.js
@@ -1,6 +1,49 @@
-var BaseView = require('../base');
+var BaseView = require('../base')
+  , _ = require('underscore')
+  , User = require('../../models/user');
 
 module.exports = BaseView.extend({
-    className: 'users-create-view'
+    className: 'users-create-view',
+    events: {
+        'submit form': 'save'
+    },
+    initialize: function() {
+        if (!this.model) {
+            this.model = new User({}, { app: this.app });
+        }
+    },
+    renderErrors: function(model, xhr) {
+        // render errors only when they are sent
+        if (xhr.status !== 400) { return; }
+        var errors = xhr.body;
+
+        this.$('.help-block').remove();
+        this.$('.has-error').removeClass('has-error');
+        _.each(errors, function(error) {
+            var input = $('[name="' + error.field + '"]');
+            input.parents('.form-group').addClass('has-error');
+            $('<span>', { "class": "help-block", text: error.message })
+                .appendTo(input.parent());
+        });
+    },
+    save: function(event) {
+        var form = this.$(event.target)
+          , values = {};
+
+        event.preventDefault();
+        _.each(form.serializeArray(), function(item) {
+            values[item.name] = item.value
+        });
+
+        this.model.save(values, {
+            success: _.bind(this.goToList, this),
+            error:   _.bind(this.renderErrors, this)
+        });
+    },
+    goToList: function() {
+        // @todo: add alert
+//        Alert.success('User was successfully saved');
+        this.app.router.redirectTo('/users');
+    }
 });
 module.exports.id = 'users/create';
--- app/views/users/partials/form.js
+++ app/views/users/partials/form.js
@@ -1,52 +1,6 @@
-var BaseView = require('../../base')
-  , _ = require('underscore')
-  , User = require('../../../models/user');
+var BaseView = require('../../base');
 
 module.exports = BaseView.extend({
-    className: 'users-partials-form-view',
-    events: {
-        'submit form': 'save'
-    },
-    initialize: function() {
-        this.model = new User();
-
-        // @todo: assign fetcher to model in better way
-        // assign app to model
-        // model cannot be saved without it
-        this.model.app = this.app;
-    },
-    renderErrors: function(model, xhr) {
-        // render errors only when they are sent
-        if (xhr.status !== 400) { return; }
-        var errors = xhr.body;
-
-        this.$('.help-block').remove();
-        this.$('.has-error').removeClass('has-error');
-        _.each(errors, function(error) {
-            var input = $('[name="' + error.field + '"]');
-            input.parents('.form-group').addClass('has-error');
-            $('<span>', { "class": "help-block", text: error.message })
-                .appendTo(input.parent());
-        });
-    },
-    save: function(event) {
-        var form = this.$(event.target)
-          , values = {};
-
-        event.preventDefault();
-        _.each(form.serializeArray(), function(item) {
-            values[item.name] = item.value
-        });
-
-        this.model.save(values, {
-            success: _.bind(this.goToList, this),
-            error:   _.bind(this.renderErrors, this)
-        });
-    },
-    goToList: function() {
-        // @todo: add alert
-//        Alert.success('User was successfully saved');
-        this.app.router.redirectTo('/users');
-    }
+    className: 'users-partials-form-view'
 });
 module.exports.id = 'users/partials/form';
