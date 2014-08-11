d4899c0eae2b5b6e7c23c388c9ce8efe0c34f63f
Ivan Nosov <i.k.nosov@gmail.com>
2014-08-11 19:46:08 +0300

    use leading $

--- app/views/app_view.js
+++ app/views/app_view.js
@@ -3,17 +3,17 @@ var BaseAppView = require('rendr/client/app_view');
 
 module.exports = BaseAppView.extend({
     initialize: function() {
-        var links = this.$('.nav a')
-          , loader = this.$('#loader');
+        var $links = this.$('.nav a')
+          , $loader = this.$('#loader');
 
         this.app.on('change:loading', function(app, loading) {
-            loading ? loader.removeClass('hide')
-                    : loader.addClass('hide');
+            loading ? $loader.removeClass('hide')
+                    : $loader.addClass('hide');
         });
 
         this.app.router.on('action:start', function(route) {
-            links.parent().removeClass('active');
-            links.each(function() {
+            $links.parent().removeClass('active');
+            $links.each(function() {
                 var $el = $(this)
                   , action = $el.data('action')
                   , controller = $el.data('controller');
