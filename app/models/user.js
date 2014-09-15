225d35fd85288c74aa1c01343ae88b06d954fe7b
Ivan Nosov <i.k.nosov@gmail.com>
2014-09-15 11:35:08 +0300

    rename _id to id

--- app/models/user.js
+++ app/models/user.js
@@ -2,14 +2,11 @@ var Base = require('./base');
 
 module.exports = Base.extend({
   url: function() {
-      // @todo: do something with it..
       if (this.get('id')) {
           return '/users/:id';
-      } else if (this.get('_id')) {
-          return '/users/:_id';
       }
       return '/users';
   },
-  idAttribute: '_id'
+  idAttribute: 'id'
 });
 module.exports.id = 'User';
